3ad0bf4c18e317a0222125472936dc4c3c026c98
Fariba <faribashakeri81@gmail.com>
2025-07-04 12:44:15 +0330

    about added

--- blueprints/general.py
+++ blueprints/general.py
@@ -5,3 +5,7 @@ app = Blueprint("general", __name__)
 @app.route('/')
 def hello_world():
     return 'helloww world! this is the main page'
+
+@app.route('/about')
+def about():
+    return 'about us'
