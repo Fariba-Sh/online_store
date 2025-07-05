1d54dbeff4d7221dcd5ee3d1d52cc7c323fbe80c
Fariba <faribashakeri81@gmail.com>
2025-07-05 17:43:30 +0330

    templates added and admin bluprint(with app.py , config.py) has been modified

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,8 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, session , redirect , request , abort
+import config
+
 
 app = Blueprint("admin", __name__)
 
-@app.route('/admin')
-def admin():
-    return 'Admin page'
+@app.route('/admin' , methods = ["POST" , "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username", None)
+        password = request.form.get("password" , None)
+    
+        if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD :
+            session['admin_login'] = username
+            return redirect("admin/dashboard")
+        else:
+            return redirect("admin")
 
+    
+    else:
+        return render_template("admin/login.html")
+
+
+@app.route('/admin/dashboard' , methods = ["GET"])
+def dashboard1():
+    if session.get('admin_login' , None) == None:
+        abort(403)
+        # abort 403 :forbiden
+    return "this is your dashboard"
