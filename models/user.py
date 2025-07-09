9fd94bb15d437a96e21cbb0fae51503739025442
Fariba <faribashakeri81@gmail.com>
2025-07-09 10:05:55 +0330

    user.html(login and register) has been added , flask login installed and backend(user:blueprint,models,app.py) has been modified

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 from sqlalchemy import *
 from extentions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable = False, index= True)
     password = Column(String,  nullable = False, index= True)
-    email = Column(String, index= True)
     phone = Column(String(11), nullable = False, index= True)
-    adress = Column(String, nullable = False, index= True)
+    address = Column(String, nullable = False, index= True)
